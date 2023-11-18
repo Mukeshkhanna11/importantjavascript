46cd153369f7b25bfa7cda56ea1890df9261cda3
Mukeshkhanna11 <mukeshmass.9865@gmail.com>
2023-11-18 19:24:51 +0530

    api concept added

--- script.js
+++ script.js
@@ -44,3 +44,13 @@ let arr = [4, 6, 2, 3, 1, 1, 3, 5, 7, 8, 4, 3];
 
 let array2 = arr.filter((val, index) => arr.indexOf(val) == index);
 console.log(array2);
+//api
+
+fetch("https://official-joke-api.appspot.com/jokes/programming/random")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data["0"].setup, data["0"].punchline);
+  })
+  .catch((err) => {
+    console.log("err");
+  });
